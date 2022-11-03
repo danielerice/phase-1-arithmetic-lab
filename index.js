61e408c618f46a56e4694092788945366dd03057
Daniel Rice <danielrice5627@gmail.com>
2022-11-02 23:06:09 -0700

    commit tree f2d35840f486d910c88f3364c6fe4a7befa7b092

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1=31;
+let num2=2;
+let num3=24;
+let num4=5;
 
-// Write your code here
+
+
+let multiply= num1*num2;
+let random= Math.floor(Math.random()*10)+1;
+let mod= (num3)%(num4);
+let max= Math.max(1,4,5,6,7,8,9,20);
